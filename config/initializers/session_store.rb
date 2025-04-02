fb45dd9d2bb6b1bf3e4a5374d48b6e70a457831f
Joe Sak <joe@joesak.com>
2025-04-02 10:49:14 -0600

    Try lax cookie

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,4 +2,4 @@ Rails.application.config.session_store :cookie_store,
   key: "_bounty_hunter_session",
   domain: Rails.env.local? ? :all : "bounty-hunter-5sel.onrender.com",
   secure: Rails.env.production?,
-  same_site: :strict
+  same_site: :lax
