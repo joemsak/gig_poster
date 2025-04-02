1382ec2b44cd5866ed0ee3a7b3363e8d413dc64e
Joe Sak <joe@joesak.com>
2025-04-02 10:13:45 -0600

    Fix local session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 Rails.application.config.session_store :cookie_store,
   key: "_bounty_hunter_session",
-  domain: "bounty-hunter-5sel.onrender.com",
+  domain: Rails.env.local? ? :all : "bounty-hunter-5sel.onrender.com",
   secure: Rails.env.production?,
   same_site: :strict
