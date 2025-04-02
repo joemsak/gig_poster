35e80de8d84053ce24c812fa529d89fda6d176e1
Joe Sak <joe@joesak.com>
2025-04-02 09:25:55 -0600

    Try session cookie store IDK

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,5 @@
+Rails.application.config.session_store :cookie_store, 
+  key: "_bounty_hunter_session",
+  domain: "bounty-hunter-5sel.onrender.com"
+  secure: Rails.env.production?,
+  same_site: :strict
